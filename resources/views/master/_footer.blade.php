60dff43adf0e447643411da9e6c3b4fbf2ab4aed
jason-vp <jason_vp8@hotmail.com>
2016-05-19 22:35:06 +0200

    Transition to Laravel 5.2 up and working!

---
+++ resources/views/master/_footer.blade.php
@@ -0,0 +1,12 @@
+</div>
+
+<footer>
+    <nav>
+        <a href='#hPrinc'><img src="/images/a_back_to_top.png"></a>
+    </nav>
+</footer>
+@yield('autoScripts')
+
+</body>
+
+</html>
---
+++ resources/views/master/mainLayout.blade.php
@@ -0,0 +1,6 @@
+@include('master._head')
+@include('master._mainHeader')
+
+@yield('content')
+
+@include('master._footer')
